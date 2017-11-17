00a7f49a907558f1e4de5fb1832315b287221d48
Jonathan Berkhahn <jaberkha@us.ibm.com>
2017-11-16 17:15:00 -0800

    Inital commit

---
+++ cmd/class/class.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jberkhahn/service-catalog-plugins/pkg/utils"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const usage = `Usage:
+  kubectl plugin class SUBCOMMAND
+
+Available subcommands:
+  list
+  get
+`
+
+const getUsage = `Usage:
+  kubectl plugin class get CLASSNAME
+`
+
+func main() {
+	if len(os.Args) < 2 {
+		utils.Exit1(usage)
+	}
+
+	scClient, _ := utils.NewClient()
+	if os.Args[1] == "list" {
+		classes, err := scClient.ServicecatalogV1beta1().ClusterServiceClasses().List(v1.ListOptions{})
+		if err != nil {
+			utils.Exit1(fmt.Sprintf("Unable to list classes (%s)", err))
+		}
+
+		table := utils.NewTable("CLASS NAME", "NAMESPACE", "BROKER NAME")
+		for _, v := range classes.Items {
+			table.AddRow(v.Name, v.Namespace, v.Spec.ClusterServiceBrokerName)
+			err = table.Print()
+		}
+		if err != nil {
+			utils.Exit1(fmt.Sprintf("Error printing result (%s)", err))
+		}
+	} else if os.Args[1] == "get" {
+		if len(os.Args) != 3 {
+			utils.Exit1(getUsage)
+		}
+		className := os.Args[2]
+		class, err := scClient.ServicecatalogV1beta1().ClusterServiceClasses().Get(className, v1.GetOptions{})
+		if err != nil {
+			utils.Exit1(fmt.Sprintf("Unable to find class %s (%s)", className, err))
+		}
+		table := utils.NewTable("CLASS NAME", "NAMESPACE", "BROKER NAME")
+		table.AddRow(class.Name, class.Namespace, class.Spec.ClusterServiceBrokerName)
+		err = table.Print()
+	} else {
+		utils.Exit1(usage)
+	}
+}
